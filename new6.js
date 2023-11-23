913cd46b4584041a8d32429345f5ef83f5507a76
SanOP360 <sanjayl2001alm@gmail.com>
2023-11-23 22:33:00 +0530

    loaded data from the sever on loading

--- new6.js
+++ new6.js
@@ -1,43 +1,11 @@
-// const axios= require('axios');
-
 const myform = document.querySelector("#myform");
 const myName = document.querySelector("#name");
 const myEmail = document.querySelector("#email");
 const myPhoneno = document.querySelector("#phoneNo");
 const users = document.querySelector(".items");
 
-myform.addEventListener("submit", onSubmit);
-
-function onSubmit(e) {
-  e.preventDefault();
-
-  const UserDetails = {
-    name: myName.value,
-    email: myEmail.value,
-    phoneNo: myPhoneno.value,
-  };
-
-//   const userDetailsString = JSON.stringify(UserDetails);
-//   localStorage.setItem(myEmail.value, userDetailsString);
-
-axios
-  .post(
-    "https://crudcrud.com/api/bd442d3d78904226a9427ea7d655460e/appointData",
-    UserDetails,
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  )
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-
+// Function to create a new list item and append it to the list
+function createElement(userData) {
   var button = document.createElement("button");
   button.textContent = "Delete";
   button.style.backgroundColor = "red";
@@ -53,7 +21,7 @@ axios
   const li = document.createElement("li");
   li.appendChild(
     document.createTextNode(
-      `Name: ${UserDetails.name}, Email: ${UserDetails.email}, Phone NO: ${UserDetails.phoneNo}`
+      `Name: ${userData.name}, Email: ${userData.email}, Phone NO: ${userData.phoneNo}`
     )
   );
 
@@ -62,22 +30,83 @@ axios
   users.appendChild(li);
 
   button.addEventListener("click", function () {
-    const emailToRemove = UserDetails.email;
-    // localStorage.removeItem(emailToRemove);
-    button.parentNode.remove();
+    //  delete action here
+    
+    const emailToRemove = userData.email;
+    // axios.delete("https://crudcrud.com/api/bd442d3d78904226a9427ea7d655460e/appointData/" + emailToRemove)
+    //   .then((response) => {
+    //     console.log(response);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+    li.remove();
   });
-  editBtn.addEventListener("click", function () {
-    const emailToRemove = UserDetails.email;
-    const nameToedit = UserDetails.name;
-    const phoneNUmberToEdit = UserDetails.phoneNo;
-    // localStorage.removeItem(emailToRemove);
-    myEmail.value = emailToRemove;
-    myPhoneno.value = phoneNUmberToEdit;
-    myName.value = nameToedit;
 
-    editBtn.parentNode.remove();
+  editBtn.addEventListener("click", function () {
+    // Handle the edit action here
+    // For example: populate the form fields with the data for editing
+    myName.value = userData.name;
+    myEmail.value = userData.email;
+    myPhoneno.value = userData.phoneNo;
+
+    // Remove the list item from the DOM
+    li.remove();
   });
+}
+
+myform.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const UserDetails = {
+    name: myName.value,
+    email: myEmail.value,
+    phoneNo: myPhoneno.value,
+  };
+
+  axios
+    .post(
+      "https://crudcrud.com/api/bd442d3d78904226a9427ea7d655460e/appointData",
+      UserDetails,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then((response) => {
+      console.log(response);
+
+      // Add the new data to the list
+      createElement(UserDetails);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // Reset form fields
   myName.value = "";
   myEmail.value = "";
   myPhoneno.value = "";
 }
+
+// Load existing data from the server on page load
+window.addEventListener("DOMContentLoaded", () => {
+  axios
+    .get(
+      "https://crudcrud.com/api/bd442d3d78904226a9427ea7d655460e/appointData"
+    )
+    .then((response) => {
+      console.log(response);
+
+      // Loop through existing data and create list items
+      for (var i = 0; i < response.data.length; i++) {
+        createElement(response.data[i]);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
